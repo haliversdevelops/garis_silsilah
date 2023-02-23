5614e014cd1839e31ee0e6c94f4038fdca0eac1f
haliversdevelops <halimansyahputra@gmail.com>
2023-02-23 18:13:34 +0700

    Migrate Bootstrap V3 to V4

--- resources/views/users/partials/edit_profile.blade.php
+++ resources/views/users/partials/edit_profile.blade.php
@@ -4,14 +4,13 @@
         {!! FormField::text('name', ['label' => __('user.name')]) !!}
         {!! FormField::text('nickname', ['label' => __('user.nickname')]) !!}
         <div class="row">
-            <div class="col-md-6">{!! FormField::radios('gender_id', [1 => __('app.male_code'), __('app.female_code')], ['label' => __('user.gender')]) !!}</div>
-            <div class="col-md-4">
-                {!! FormField::text('birth_order', ['label' => __('user.birth_order'), 'type' => 'number', 'min' => 1]) !!}
+            <div class="col-md-7">{!! FormField::radios('gender_id', [1 => __('app.male_code'), __('app.female_code')], ['label' => __('user.gender')]) !!}</div>
+            <div class="col-md-7">{!! FormField::text('birth_order', ['label' => __('user.birth_order'), 'type' => 'number', 'min' => 1]) !!}
             </div>
         </div>
-        <div class="row">
+        <div class="row" style="display:flex">
             <div class="col-md-6">{!! FormField::text('yob', ['label' => __('user.yob'), 'placeholder' => __('app.example').' 1959']) !!}</div>
-            <div class="col-md-6">{!! FormField::text('dob', ['label' => __('user.dob'), 'placeholder' => __('app.example').' 1959-07-20']) !!}</div>
+            <div class="col-md-7">{!! FormField::text('dob', ['label' => __('user.dob'), 'placeholder' => __('app.example').' 1959-07-20']) !!}</div>
         </div>
     </div>
 </div>
