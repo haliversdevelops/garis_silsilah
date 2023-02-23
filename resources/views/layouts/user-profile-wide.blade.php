925cc631dccddda3b05693c3278b8e588c12268e
haliversdevelops <halimansyahputra@gmail.com>
2023-02-23 19:00:02 +0700

    Migrate Bootstrap v.3 to v.4

--- resources/views/layouts/user-profile-wide.blade.php
+++ resources/views/layouts/user-profile-wide.blade.php
@@ -4,7 +4,7 @@
 </div>
 <div class="container">
     @include('users.partials.action-buttons', ['user' => $user])
-    <h2 class="page-header">
+    <h2 class="page-header mt-3">
         {{ $user->name }} <small>@yield('subtitle')</small>
     </h2>
     @yield('user-content')
